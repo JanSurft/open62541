06d8365a6ea73b7fb74f5de439279fa7be7ebbd4
Jan Hermes <jan@hermes-technology.de>
2021-10-06 12:25:21 +0200

    feat(client): add eventloop to config

--- include/open62541/client.h
+++ include/open62541/client.h
@@ -28,6 +28,8 @@
 #include <open62541/plugin/network.h>
 #include <open62541/plugin/securitypolicy.h>
 
+#include <open62541/plugin/eventloop.h>
+
 _UA_BEGIN_DECLS
 
 /**
@@ -119,6 +121,9 @@ typedef struct {
                                               * up together with the
                                               * configuration. So it is possible
                                               * to allocate them on ROM. */
+    /* EventLoop */
+    UA_EventLoop *eventLoop;
+    UA_Boolean externalEventLoop; /* The EventLoop is not deleted with the config */
 
     /* Available SecurityPolicies */
     size_t securityPoliciesSize;
